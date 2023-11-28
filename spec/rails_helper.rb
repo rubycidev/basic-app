1803af70146634b53d35fd043e22c6c353867b64
rubycidev <117899216+rubycidev@users.noreply.github.com>
2023-11-28 16:31:04 +0100

    Update rails_helper.rb to test .env.test variable

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+ENV['TEST_DOT_ENV_DOT_TEST'] ||='env not readable from .env.test file'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
