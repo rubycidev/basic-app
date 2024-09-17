190b8c486be4e18f96c8618b03faba65af1d0bc6
rubycidev <117899216+rubycidev@users.noreply.github.com>
2024-09-17 20:20:21 +0200

    added 2 failed specs

--- spec/routing/items_routing_spec.rb
+++ spec/routing/items_routing_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 RSpec.describe ItemsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/items").to route_to("items#index")
+      expect(get: "/items").not_to route_to("items#index")
     end
 
     it "routes to #new" do
-      expect(get: "/items/new").to route_to("items#new")
+      expect(get: "/items/new").to route_to("items#edit")
     end
 
     it "routes to #show" do
